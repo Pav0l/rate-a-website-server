e8903276f9e6f76a9f45e115b013be945b9bffda
Pav0l <pavol.karas@gmail.com>
2019-05-06 21:42:10 +0200

    Setup for Jest tests

---
+++ tests/rating.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const server = require('../api/server');
+const db = require('../data/db');
+
+describe('Rating resource', () => {
+  beforeEach(() => {
+    
+  })
+})

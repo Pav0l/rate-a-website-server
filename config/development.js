440d203840833fa691e2111c67c62b3e79105ac3
Pav0l <pavol.karas@gmail.com>
2019-05-07 22:03:16 +0200

    Remove config files

--- config/development.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  knex: {
-    client: 'sqlite3',
-    connection: {
-      filename: './data/survey.sqlite3',
-    },
-    useNullAsDefault: true,
-  },
-};
--- config/test.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  knex: {
-    client: 'sqlite3',
-    connection: {
-      filename: './data/test.sqlite3',
-    },
-    useNullAsDefault: true,
-  },
-};

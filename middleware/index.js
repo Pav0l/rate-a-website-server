c87f3d3aa8eb52e7c93b45e9b5dc18bef5bee8ae
Pav0l <pavol.karas@gmail.com>
2019-05-11 08:51:03 +0200

    Add cors options

--- middleware/index.js
+++ middleware/index.js
@@ -3,10 +3,14 @@ const compression = require('compression');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const corsOptions = {
+  'Access-Control-Allow-Origin': '*',
+};
+
 // line up third pary middleware
 module.exports = server => {
   server.use(helmet());
   server.use(compression());
-  server.use(cors());
+  server.use(cors(corsOptions));
   server.use(express.json());
 };

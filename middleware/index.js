7445c31a5428ba5d86f6e0a00a0557a79bd86693
Pav0l <pavol.karas@gmail.com>
2019-05-30 17:12:30 +0200

    Update CORS origin whitelist

--- middleware/index.js
+++ middleware/index.js
@@ -3,16 +3,28 @@ const compression = require('compression');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const whitelist = [
+  'moz-extension://8c764801-4e5d-403e-a0d8-69c226c13712',
+  'chrome-extension://akdeaedbblfhendjfkgmkkbmbfjfobpk',
+  'chrome-extension://oplipkhodadjellpakfommojjmcikidf'
+];
 const corsOptions = {
   // specify requests from which origin are allowed to use resources on server
   // origin: 'chrome-extension://akdeaedbblfhendjfkgmkkbmbfjfobpk',
   // which methods fron origin are allowed
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['POST', 'OPTIONS', 'GET'],
   allowedHeaders: [
     'Content-Type',
     'Access-Control-Request-Method',
-    'Access-Control-Allow-Headers',
-  ],
+    'Access-Control-Allow-Headers'
+  ]
 };
 
 // line up third party middleware

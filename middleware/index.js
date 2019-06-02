24e9d813cb696a5e862b79cc0e80c9d6f8f801e6
Pav0l <pavol.karas@gmail.com>
2019-06-02 21:03:16 +0200

    Enable CORS origin opts

--- middleware/index.js
+++ middleware/index.js
@@ -10,13 +10,13 @@ const whitelist = [
   'https://happiest-websites.netlify.com'
 ];
 const corsOptions = {
-  //   origin: function(origin, callback) {
-  //     if (whitelist.indexOf(origin) !== -1) {
-  //       callback(null, true);
-  //     } else {
-  //       callback(new Error('Not allowed by CORS'));
-  //     }
-  //   },
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['POST', 'OPTIONS', 'GET'],
   allowedHeaders: [
     'Content-Type',
